49b1e96a56e0fa0969c64fb07fdf2f5231a68828
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2020-01-29 10:40:06 +0900

    fix-code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,19 @@
 class UsersController < ApplicationController
   before_action :user_admin, only: [:index]
 
+  def index
+    @users = User.all
+    @comments = Comment.all
+    @comment = Comment.find(params[:id])
+  end
+
   def show
-    
     if user_signed_in?
       @user = current_user
       @favorite = Favorite.where("user_id = ?", @user)
     else 
       redirect_to edit_user_registration_path
     end
-
-  end
-
-  def index
-    @users = User.all
-    @comments = Comment.all
-    @comment = Comment.find(params[:id])
   end
 
   private
@@ -27,5 +25,4 @@ class UsersController < ApplicationController
         render action: "index"
       end
     end
-
 end
